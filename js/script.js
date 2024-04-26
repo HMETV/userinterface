310ec9a9542096924543c691b44db8a3c007ebdd
Diego V <diego@codewith.mobi>
2024-04-25 20:32:46 -0500

    feat: add backward button

--- js/script.js
+++ js/script.js
@@ -12,6 +12,24 @@ function startAnimation1() {
     // Code to start the animation goes here
     // For example, you can use CSS animations or a JavaScript animation library
     const element = document.querySelector('.robot');
-    element.style.animation = 'slide 1s forwards';
+    element.style.animation = 'forward 1s forwards';
+}
+
+
+// Get the button element
+const button2 = document.querySelector('#backward-button');
+
+// Add a click event listener to the button
+button2.addEventListener('click', () => {
+    // Start the animation
+    startAnimation2();
+});
+
+// Function to start the animation
+function startAnimation2() {
+    // Code to start the animation goes here
+    // For example, you can use CSS animations or a JavaScript animation library
+    const element = document.querySelector('.robot');
+    element.style.animation = 'backward 1s backwards';
 }
 
